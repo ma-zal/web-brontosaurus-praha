a12a54526f0317a4bbf168220ead3b93c73ddceb
Michal Švarný <michal.svarny@gmail.com>
2016-03-22 01:10:26 +0100

    Update index.php
    Nový text od Rozruchu

--- web/kdo-jsme/rozruch/index.php
+++ web/kdo-jsme/rozruch/index.php
@@ -16,14 +16,16 @@ require_once __DIR__ . '/../../config/html_top.php';
 
 	<p>Jsme parta kamarádů se společným zájmem o přírodu, neobyčejné zážitky a trochu méně konzumní styl života.</p>
 
-	<p>Kromě ochrany přírody se zaměřujeme na neformální vzdělávání mládeže, zejména budování vztahů mezi lidmi a&nbsp;přírodou a mezi lidmi navzájem.</p>
+	<p>Na našich akcích je hlavním cílem ochrana přírody a neformální vzdělávání mládeže, zejména budování vztahů mezi lidmi a přírodou a mezi lidmi navzájem. V našem programu tak nechybí nejrůznější hry a aktivity na rozvoj individuálních, týmových schopností, prostor pro vlastní realizaci, ale taky pro odreagování a vyblbnutí, protože přece jsme a děláme rozruch.</p>
 
 	<p>
-		Ale o tom, kdo skutečně jsme, se nejlépe přesvědčíte, když se podíváte na akce které děláme, nebo dokonce na nějakou přijedete.
-		Sídlíme v Praze, tedy většinu krátkých akcí situujeme poblíž, na prázdninovky rádi jezdíme hlavně do Krkonoš či Jeseníků.
+		Ale o tom, kdo skutečně jsme, se nejlépe přesvědčíte, když se podíváte na akce, které děláme, nebo na ně rovnou přijedete. </p>
+	<p>
+		Sídlíme v Praze, tedy většinu krátkých, většinou víkendových akcí situujeme poblíž, ale spoustu z nás pochází z nejrůznějších končin, takže to není žádné pravidlo. Je ale pravda, že v Praze každoročně akce konáme, protože v pražských parcích věšíme ptačí budky, o které se pravidelně staráme. <br>
+		Na delší prázdninové akce jezdíme hlavně do Krkonoš či Jeseníků. Kosíme tady louky, pomáháme s obnovou zaniklé osady, dojíme kozy, spíme pod plachtou, myjeme se v potoce, vaříme na ohni a spoustu dalších úžasných věcí. 
 	</p>
 
-	<p>Chceš-li se dozvědět o nás víc, podívej se na náš <a href="http://rozruch.brontosaurus.cz/" target="_blank">web brontosauřího článku Rozruch</a>.
+	<p>Další informace se o nás dozvíš na <a href="http://rozruch.brontosaurus.cz/" target="_blank">webu základního článku Rozruch</a>.
 
 	<ul class="ilustracniFotoSeznam">
 		<li class="img-responsive img-responsive-50"><img src="img/img2581.jpg" alt="Ilustrační foto" /></li>
