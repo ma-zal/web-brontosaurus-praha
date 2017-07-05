ec0275894005b9731f4e9d2223aed29147f877d8
ma-zal <ma-zal@users.noreply.github.com>
2017-07-05 11:57:09 +0200

    Aktualizace organizátorského kurzu

--- web/organizatorsky-kurz/index.php
+++ web/organizatorsky-kurz/index.php
@@ -9,27 +9,41 @@ $GLOBALS[CONFIG]['leftMenu']['organizatorsky-kurz']['aktivni'] = true;
 require_once __DIR__ . '/../config/html_top.php';
 ?>
 
-    <h1>O organizátorském kurzu</h1>
-
-    <p>
-        <em>Organizátorský kurz pořádaný Hnutím Brontosaurus probíhá letos vždy ve středu od 18 do 21 hodin od poloviny října až do dubna následujícího roku. Ačkoliv letošní ročník již začal, v průběhu celého kurzu je možné se za drobný poplatek 40&nbsp;Kč zúčastnit i jeho jednotlivých seminářů.</em>
-    </p>
-
     <ul class="ilustracniFotoSeznam">
         <li class="img-responsive img-responsive-100"><img src="img/ohb-plakat.png" /></li>
     </ul>
 
+    <h1>O organizátorském kurzu</h1>
+
     <p>
-        Kurz je určen pro začínající organizátory zážitkových akcí především pro mládež, ale díky širokému záběru a praktickému zaměření bude přínosný i pro zkušené organizátory. Úspěšní absolventi mohou získat osvědčení "Hlavní vedoucí dětských táborů" akreditovaný Ministerstvem školství.
+        Organizátorský kurz je každoroční 8-mi měsíční kurz určený hlavně začínajícím a mírně pokročilým organizátorům zážitkových
+        akcí pro děti, mládež i dospělé. Díky širokému záběru a praktickému zaměření bude přínosný i pro zkušené organizátory.
     </p>
     <p>
-        Hnutí Brontosaurus již přes 40 let pořádá tábory pro děti a mládež, jejichž součástí vedle zážitkových a sportovních programů je i dobrovolnická práce na pomoc památkám a přírodě v ČR i v zahraničí. Organizátorský kurz je vynikající příležitostí, jak z těchto dlouholetých zkušeností čerpat.
+        Kurz probíhá formou neformálních večeních seminářů, které se konají vždy jednou týdně v době 18:00-21:00.
+        Kurz obsahuje celkem 24 takových seminářů, každý na jiné téma. V rámci kurzu jsou pořádány také tři víkendové akce,
+        na kterých si účast i organizování zážitkových akcí mohou účastníci vyzkoušet na vlastní kůži.
+    </p>
+
+
+    <h2>Proč kurz u nás?</h2>
+
+    <p>
+        Hnutí Brontosaurus již přes 40 let pořádá tábory pro děti a mládež, jejichž součástí vedle zážitkových
+        a sportovních programů je i dobrovolnická práce na pomoc památkám a přírodě v ČR i v zahraničí.
+        Organizátorský kurz je vynikající příležitostí, jak z těchto dlouholetých zkušeností čerpat.
     </p>
     <p>
-        Díky dobrovolnické práci organizátorů i většiny lektorů a díky podpoře Ministerstva školství ČR Vám můžeme kurz o rozsahu 24 tříhodinových seminářů nabídnout za velmi výhodnou cenu 900 Kč. Součástí kurzu jsou také 2-3 víkendové akce, na kterých si účast i organizování zážitkových akcí mohou účastníci vyzkoušet na vlastní kůži.
+        Kurz se každoročně koná hlavně díky dobrovolnické práci organizátorů i většiny lektorů a díky podpoře Ministerstva školství ČR.
+        Díky tomu, že naši organizátoři jsou motivování svým nadšením pro dobrou věc, dokážeme nabídnout prostředí, které
+        žádná podobná komerčně konaná akce nabídnout nedokáže.
     </p>
+
+
+    <h2>Více informací?</h2>
     <p>
-        Více informací a podrobný program naleznete na webu <a href="http://praha.brontosaurus.cz/ohb/">pražského Organizátorského kurzu Hnutí Brontosaurus</a>.
+        Detaily a podrobný program nalezneš na vlastním webu
+        <a href="https://sites.google.com/brontosaurus.cz/organizatorsky-kurz-praha/">pražského Organizátorského kurzu Hnutí Brontosaurus</a>.
     </p>
 
     <ul class="ilustracniFotoSeznam">
